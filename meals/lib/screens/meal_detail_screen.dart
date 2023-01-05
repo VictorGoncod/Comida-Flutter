8ea28d93fdba07ae4a7f2d0e2b3f631d80cb059a
Victor <victo.goncalves@hotmail.com>
2023-01-05 18:07:43 -0300

    ingedientes

--- meals/lib/screens/meal_detail_screen.dart
+++ meals/lib/screens/meal_detail_screen.dart
@@ -11,8 +11,49 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Center(
-        child: Text('Detalhes da Refeição'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredientes',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 200,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
+                      child: Text(meal.ingredients[index]),
+                    ),
+                    color: Theme.of(context).colorScheme.secondary,
+                  );
+                }),
+          )
+        ],
       ),
     );
   }
